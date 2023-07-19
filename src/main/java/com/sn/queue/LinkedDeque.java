228990f79cd5e4c9ab010df698e7702ba41f10df
Mr-linWait <1446681329@qq.com>
2023-07-19 21:24:00 +0800

    1.基于链表实现的双向队列 2.Deque接口扩充双向操作方法

--- src/main/java/com/sn/queue/LinkedDeque.java
+++ src/main/java/com/sn/queue/LinkedDeque.java
@@ -16,7 +16,95 @@ public class LinkedDeque<E> implements Deque<E> {
     }
 
     public LinkedDeque(){
-        this(null,null);
+       // this(null,null);
+    }
+
+
+    /**
+     * 尾插法
+     * @param element
+     */
+    public void enqueue(E element) {
+        DLNode newNode = new DLNode(element);
+        if (isEmpty()) {
+            head = newNode;
+            tail =newNode;
+        }else {
+            tail.next=newNode;
+            newNode.previous=tail;
+            tail=newNode;
+        }
+    }
+
+    /**
+     * 头部出队
+     * @return
+     */
+    public E dequeue() {
+        E e = peek();
+        DLNode next = head.next;
+        if (next==null)
+            tail=null;
+        else
+            next.previous=null;
+        head=next;
+        return e;
+    }
+
+    public E peek() {
+        if (isEmpty())
+            throw new NullPointerException("LinkedDeque Object is null");
+        return head.data;
+    }
+
+    public boolean isEmpty() {
+        return (head==null && tail ==null);
+    }
+
+    public void clear() {
+        head=null;
+        tail=null;
+    }
+
+    public void addFirst(E element) {
+        DLNode newNode = new DLNode(element);
+        if (isEmpty()){
+            head=newNode;
+            tail=newNode;
+        }else {
+            newNode.next=head;
+            head.previous=newNode;
+            head=newNode;
+        }
+    }
+
+    public void addLast(E element) {
+        enqueue(element);
+    }
+
+    public E removeFirst() {
+        return dequeue();
+    }
+
+    public E removeLast() {
+        E e = peekLast();
+        DLNode previous = tail.previous;
+        if (previous==null)
+            head=null;
+        else
+            previous.next=null;
+        tail=previous;
+        return e;
+    }
+
+    public E peekFirst() {
+        return peek();
+    }
+
+    public E peekLast() {
+        if (isEmpty())
+            throw new NullPointerException("LinkedDeque Object is null");
+        return tail.data;
     }
 
     private class DLNode{
