a3780eeec6a0eb970a6c7cc0222e913531b5ed98
Mr-linWait <1446681329@qq.com>
2023-08-29 19:01:09 +0800

    1.1114. 按序打印

---
+++ src/main/java/com/sn/leetcode/SolutionsPrint.java
@@ -0,0 +1,27 @@
+package com.sn.leetcode;
+
+
+/**
+ * 1114. 按序打印
+ */
+public class SolutionsPrint {
+    private int sort=0;
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
